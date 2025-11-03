985953326267aaa96fecaaed534299be84997d23
helges-ui <helge.schindler@gmail.com>
2025-11-03 22:21:57 +0100

    refactoring updated permission system

--- daydo/permissions.py
+++ daydo/permissions.py
@@ -4,6 +4,7 @@ These permissions enforce the role-based access control defined in the product b
 """
 from rest_framework.permissions import BasePermission
 from django.core.exceptions import PermissionDenied
+from daydo.models import ChildUserPermissions
 
 
 class IsParentPermission(BasePermission):
@@ -64,165 +65,90 @@ class FamilyMemberPermission(BasePermission):
         return True  # If no family_id in URL, allow access
 
 
-class CanInviteParentPermission(BasePermission):
+class BaseChildPermission(BasePermission):
+    """
+    Base permission class for child user permissions.
+    Allows access to parents and child users with specific permission.
+    
+    Subclasses must set the 'permission_field' attribute to specify
+    which permission field to check in ChildUserPermissions.
+    """
+    permission_field = None  # Must be set by subclasses
+    
+    def has_permission(self, request, view):
+        """Check if user has permission based on role and permission field"""
+        if not request.user.is_authenticated:
+            return False
+        
+        # Parents always have access
+        if request.user.is_parent:
+            return True
+        
+        # Child users need specific permission
+        if request.user.is_child_user:
+            try:
+                permissions = request.user.childuserpermissions
+                return getattr(permissions, self.permission_field, False)
+            except ChildUserPermissions.DoesNotExist:
+                return False
+        
+        return False
+
+
+class CanInviteParentPermission(BaseChildPermission):
     """
     Permission class for inviting parents.
     Allows access to parents and child users with invite permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_invite_parent
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_invite_parent'
 
 
-class CanDeleteChildProfilePermission(BasePermission):
+class CanDeleteChildProfilePermission(BaseChildPermission):
     """
     Permission class for deleting child profiles.
     Allows access to parents and child users with delete permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_delete_child_profile
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_delete_child_profile'
 
 
-class CanCreateTasksPermission(BasePermission):
+class CanCreateTasksPermission(BaseChildPermission):
     """
     Permission class for creating tasks.
     Allows access to parents and child users with create permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_create_tasks
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_create_tasks'
 
 
-class CanEditTaskDetailsPermission(BasePermission):
+class CanEditTaskDetailsPermission(BaseChildPermission):
     """
     Permission class for editing task details.
     Allows access to parents and child users with edit permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_edit_task_details
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_edit_task_details'
 
 
-class CanAccessAdminSettingsPermission(BasePermission):
+class CanAccessAdminSettingsPermission(BaseChildPermission):
     """
     Permission class for accessing admin settings.
     Allows access to parents and child users with admin permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_access_admin_settings
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_access_admin_settings'
 
 
-class CanSendMessagesPermission(BasePermission):
+class CanSendMessagesPermission(BaseChildPermission):
     """
     Permission class for sending messages.
     Allows access to parents and child users with message permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_send_messages
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_send_messages'
 
 
-class CanViewFamilyCalendarPermission(BasePermission):
+class CanViewFamilyCalendarPermission(BaseChildPermission):
     """
     Permission class for viewing family calendar.
     Allows access to parents and child users with calendar permission.
     """
-    
-    def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
-        
-        if request.user.is_parent:
-            return True
-        
-        if request.user.is_child_user:
-            try:
-                permissions = request.user.childuserpermissions
-                return permissions.can_view_family_calendar
-            except ChildUserPermissions.DoesNotExist:
-                return False
-        
-        return False
+    permission_field = 'can_view_family_calendar'
 
 
 class IsOwnerOrParentPermission(BasePermission):
